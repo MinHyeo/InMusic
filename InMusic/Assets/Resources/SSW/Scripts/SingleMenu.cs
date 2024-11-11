e5b7ed82cd27f05081344ccbb8457fcee58e2565
ssw0420 <tmddn5645@gmail.com>
2024-11-11 12:16:27 +0900

    UI Setting
    UI Setting

---
+++ InMusic/Assets/Resources/SSW/Scripts/SingleMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SingleMenu : MonoBehaviour
+{
+    // 전체 기능 구현 완료 후 Manager를 추가하여 수정 예정
+    public void OnClickStart()
+    {
+        Debug.Log("Start Button Clicked");
+    }
+
+    public void OnClickBack()
+    {
+        Debug.Log("Back Button Clicked");
+    }
+
+    public void OnClickOption()
+    {
+        Debug.Log("Option Button Clicked");
+    }
+}
