31080842b62ab6dc9d9eb0d1743746aa7ea8dafc
L-Ju123 <naver514411@gmail.com>
2024-11-19 17:01:49 +0900

    MainMenu
    2024.11.19
    -메인메뉴 80%진행
    -키가이드, 솔로플레이 캔버스, 멀티플레이 캔버스 연결만

---
+++ InMusic/Assets/Scripts/MainButtons.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MainButtons: MonoBehaviour
+{
+    public GameObject keySettingPanel;
+    public GameObject keyGuidePanel;
+    public GameObject mainCanvas;
+    public GameObject soloCanvas;
+    public GameObject multiCanvas;
+
+    public void OpenKeySettingPanel()
+    {
+        keySettingPanel.SetActive(true);
+    }
+
+    // Open KeyGuide Panel
+    public void OpenKeyGuidePanel()
+    {
+        keyGuidePanel.SetActive(true);
+    }
+
+    // ClosePannel
+    public void ClosePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+    // Open SoloPLay Canvas
+    public void OpenSoloCanvas()
+    {
+        mainCanvas.SetActive(false); 
+        soloCanvas.SetActive(true);
+    }
+
+    // Open MultiPLay Canvas
+    public void OpenMultiCanvas()
+    {
+        mainCanvas.SetActive(false); 
+        multiCanvas.SetActive(true);
+    }
+
+    // Quit
+    public void QuitGame()
+    {
+        Debug.Log("Game Exiting...");
+    }
+}
---
+++ InMusic/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button multiButton;
+    public Button soloButton;
+    public Button leftButton;
+    public Button rightButton;
+
+    private enum MainMenuButton { Multi, Solo }
+    private MainMenuButton currentButton;
+
+    private Animator multiAnimator;
+    private Animator soloAnimator;
+
+    private void Start()
+    {
+        multiAnimator = multiButton.GetComponent<Animator>();
+        soloAnimator = soloButton.GetComponent<Animator>();
+
+        currentButton = MainMenuButton.Solo; 
+        InitializeButtons();
+
+        leftButton.onClick.AddListener(() => ChangeButton(-1));
+        rightButton.onClick.AddListener(() => ChangeButton(1));
+    }
+
+    private void InitializeButtons()
+    {
+        multiButton.gameObject.SetActive(false);
+        soloButton.gameObject.SetActive(true);
+
+        UpdateArrowStates();
+    }
+
+    private void ChangeButton(int direction)
+    {
+        int nextIndex = (int)currentButton + direction;
+        if (nextIndex < 0 || nextIndex >= System.Enum.GetValues(typeof(MainMenuButton)).Length)
+            return; 
+
+        MainMenuButton nextButton = (MainMenuButton)nextIndex;
+
+        Button previousButton = GetButtonFromEnum(currentButton);
+        Button newButton = GetButtonFromEnum(nextButton);
+        Animator previousAnimator = GetAnimatorFromEnum(currentButton);
+        Animator newAnimator = GetAnimatorFromEnum(nextButton);
+
+        //Animation
+        string trigger = direction == -1 ? "MoveRight" : "MoveLeft";
+
+        newButton.gameObject.SetActive(true);
+        newAnimator.SetTrigger(trigger);
+        previousAnimator.SetTrigger(trigger);
+
+        StartCoroutine(DisableButtonAfterAnimation(previousButton, previousAnimator));
+
+        currentButton = nextButton;
+
+        UpdateArrowStates();
+    }
+
+    private void UpdateArrowStates()
+    {
+        leftButton.interactable = currentButton > MainMenuButton.Multi;
+        rightButton.interactable = currentButton < MainMenuButton.Solo;
+    }
+
+    private Button GetButtonFromEnum(MainMenuButton menuButton)
+    {
+        return menuButton == MainMenuButton.Solo ? soloButton : multiButton;
+    }
+
+    private Animator GetAnimatorFromEnum(MainMenuButton menuButton)
+    {
+        return menuButton == MainMenuButton.Solo ? soloAnimator : multiAnimator;
+    }
+
+    private IEnumerator DisableButtonAfterAnimation(Button button, Animator animator)
+    {
+        if (animator != null)
+        {
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        }
+        if (button != GetButtonFromEnum(currentButton))
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+}
