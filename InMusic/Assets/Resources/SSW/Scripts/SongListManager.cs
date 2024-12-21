f3d95181f359778812625cbb1c6581246dff38b5
tmddn5645 <tmddn5645@gmail.com>
2024-12-22 05:31:38 +0900

    step1
    step1

--- InMusic/Assets/Resources/SSW/Scripts/SongListManager.cs
+++ InMusic/Assets/Resources/SSW/Scripts/SongListManager.cs
@@ -1,33 +1,75 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SongListManager : MonoBehaviour
 {
-    public GameObject itemPrefab;
-    public RectTransform content;
-    public Sprite selectedSprite;
-    public Sprite defaultSprite;
 
-    public int totalItems = 30;
-    private float itemHeight = 83f;
-    private int visibleItems = 7;
-    private Queue<GameObject> itemPool = new Queue<GameObject>();
+    [Header("UI References")]
+    [SerializeField] private ScrollRect _songScrollRect;     // 스크롤 Rect
+    [SerializeField] private RectTransform _contentRect;     // 콘텐츠 영역 (Vertical Layout Group 등 부착 가능)
+    [SerializeField] private GameObject _songItemPrefab;     // 노래 아이템 프리팹
 
-    private List<string> numberTexts;
+    [Header("Settings")]
+    [SerializeField] private int _totalSongCount = 30;       // 실제 데이터(노래) 총 개수
+    [SerializeField] private int _slotCount = 13;            // 화면에 유지할 슬롯(아이템) 개수
+    [SerializeField] private float _itemHeight = 83f;        // 아이템 하나의 높이
 
-    private void Awake() {
-        numberTexts = new List<string>();
-        for(int i = 0; i < totalItems; i++) {
-            numberTexts.Add("Song " + (i + 1));
+    // 내부에서 활용될 배열 (슬롯 오브젝트 & 텍스트)
+    private GameObject[] _slotObjects;  
+    private Text[] _slotTexts;
+
+    private void Start() {
+        _contentRect.sizeDelta = new Vector2(
+            _contentRect.sizeDelta.x,
+            _itemHeight * _totalSongCount
+        );
+
+        _slotObjects = new GameObject[_slotCount];
+        _slotTexts   = new Text[_slotCount];
+
+        for (int i = 0; i < _slotCount; i++) {
+            // 슬롯 프리팹 생성 (Content 하위)
+            GameObject slotObj = Instantiate(_songItemPrefab, _contentRect);
+            _slotObjects[i] = slotObj;
+
+            // 슬롯 내부 Text 컴포넌트 참조 (노래 제목/정보 표시용)
+            Text slotText = slotObj.GetComponentInChildren<Text>();
+            _slotTexts[i] = slotText;
+
+            // Step1에선 아직 스크롤 인덱스 계산 X,
+            // 그냥 "Item #i (Step1)" 정도로 표시해둔다.
+            if (slotText != null) {
+                slotText.text = $"Item #{i + 1} (Step1)";
+            }
         }
     }
+}
+    // public GameObject itemPrefab;
+    // public RectTransform content;
+    // public Sprite selectedSprite;
+    // public Sprite defaultSprite;
 
-    public string GetNumberText(int index) {
-        if (index >= 0 && index < numberTexts.Count)
-            return numberTexts[index];
-        return string.Empty;
-    }
+    // public int totalItems = 30;
+    // private float itemHeight = 83f;
+    // private int visibleItems = 7;
+    // private Queue<GameObject> itemPool = new Queue<GameObject>();
+
+    // private List<string> numberTexts;
+
+    // private void Awake() {
+    //     numberTexts = new List<string>();
+    //     for(int i = 0; i < totalItems; i++) {
+    //         numberTexts.Add("Song " + (i + 1));
+    //     }
+    // }
+
+    // public string GetNumberText(int index) {
+    //     if (index >= 0 && index < numberTexts.Count)
+    //         return numberTexts[index];
+    //     return string.Empty;
+    // }
 
     // public GameObject itemPrefab;
     // public RectTransform content;
@@ -121,4 +163,3 @@ public class SongListManager : MonoBehaviour
     //             UpdateVisibleItems();
     //         }
     //     }
-}
