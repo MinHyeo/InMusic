28c7bd1c93a91632d3434fe3384d89d4d6e8cfc3
tmddn5645 <tmddn5645@gmail.com>
2025-01-09 06:50:25 +0900

    노래 불러오기, 하이라이트 표시
    Singleton 구현
    LoadManager구현
    LoadScene 생성
    SongListManager 수정
    스크롤 시 가운데 항목이 하이라이트 되도록 수정
    
    TODO: 좌측에 하이라이트 된 항목의 노래 상세 정보 표시

--- InMusic/Assets/Resources/SSW/Scripts/LoadManager.cs
+++ InMusic/Assets/Resources/SSW/Scripts/LoadManager.cs
@@ -1,16 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
 
-public class LoadManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+namespace SongList {
+    public class LoadManager : Singleton<LoadManager> {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public List<SongInfo> Songs { get; private set; }
+        protected override void Awake() {
+            base.Awake();
+        }
+
+        private void LoadAllSongs() {
+            Songs = new List<SongInfo>();
+
+            foreach(Song song in Enum.GetValues(typeof(Song))) {
+                SongInfo info = BmsLoader.Instance.SelectSong(song);
+                if (info != null) {
+                    Songs.Add(info);
+                    Debug.Log("Load Song: " + song);
+                } else {
+                    Debug.Log("Failed to load song: " + song);
+                }
+            }
+
+            SceneManager.LoadScene("test_SSW");
+        }
+
+        private void OnGUI() {
+            if (GUI.Button(new Rect(10, 10, 150, 100), "Load Songs")) {
+                LoadAllSongs();
+            }
+        }
+
     }
 }
--- InMusic/Assets/YMH/Scripts/Song/BmsLoader.cs
+++ InMusic/Assets/YMH/Scripts/Song/BmsLoader.cs
@@ -3,7 +3,6 @@ using System;
 using System.IO;
 using UnityEngine;
 
-
 public enum Song
 {
     sksmsdkvmsrjsEkrwlftordlslRk = 0,
