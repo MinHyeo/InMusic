114a07325f426c2bd34ff9c6ffe9d21672fa326d
swatper <67682393+swatper@users.noreply.github.com>
2025-01-21 17:09:45 +0900

    임시 저장

--- InMusic/Assets/PSH/Scripts/Music_Item.cs
+++ InMusic/Assets/PSH/Scripts/Music_Item.cs
@@ -5,25 +5,25 @@ using UnityEngine.Video;
 
 public class Music_Item : MonoBehaviour
 {
-    //배경(?)
-    [SerializeField] private Image background;
     [Header("음악 관련 정보")]
     [SerializeField] private Text title;
     [SerializeField] private Text artist;
     [SerializeField] private string length;
     [SerializeField] private Image albumArt;
     [SerializeField] private VideoClip musicVideo;
-    [SerializeField] private Image back;
-    [SerializeField] private Color wMint = new Color(104.0f, 240.0f, 235.0f, 0.08f);
-    [SerializeField] private Color sMint = new Color(57, 255, 255, 1.0f);
-    [SerializeField] private Color purple = new Color(155.0f, 48.0f, 255.0f, 1.0f);
     [Header("기록 관련 정보")]
     [SerializeField] private string score;
     [SerializeField] private string accuracy;
     [SerializeField] private string combo;
     [SerializeField] private Text rank;
-    
-    
+    [Header("Item 상태 정보")]
+    [SerializeField] private Image background;
+    [SerializeField] private Image rightCircle;
+    [SerializeField] private Color wMint = new Color(104.0f, 240.0f, 235.0f, 0.08f);
+    [SerializeField] private Color sMint = new Color(57, 255, 255, 1.0f);
+    [SerializeField] private Color purple = new Color(155.0f, 48.0f, 255.0f, 1.0f);
+
+
     #region Get/Set
     public Text Title { get { return title; } set { title = value; } }
     public Text Artist { get { return artist; } set { artist = value; } }
@@ -79,7 +79,7 @@ public class Music_Item : MonoBehaviour
     {
         
         SetColor(background, purple);
-        SetColor(back, purple);
+        SetColor(rightCircle, purple);
 
         SetAlpha(title, 1.0f);
         SetAlpha(artist, 1.0f);
@@ -91,7 +91,7 @@ public class Music_Item : MonoBehaviour
     public void ItemUnselect()
     {
         SetColor(background, wMint);
-        SetColor(back, wMint);
+        SetColor(rightCircle, wMint);
         
         SetAlpha(title, 0.5f);
         SetAlpha(artist, 0.5f);
@@ -100,18 +100,20 @@ public class Music_Item : MonoBehaviour
         SetAlpha(albumArt, 0.5f);
     }
 
-    #endregion
-
     //투명도만 바꾸기
-    void SetAlpha(Graphic target, float alpha) {
+    void SetAlpha(Graphic target, float alpha)
+    {
         Color Temp = target.color;
         Temp.a = alpha;
         target.color = Temp;
     }
 
     //색상 바꾸기
-    void SetColor(Graphic target, Color color) {
+    void SetColor(Graphic target, Color color)
+    {
         target.color = color;
     }
+
+    #endregion
 
 }
