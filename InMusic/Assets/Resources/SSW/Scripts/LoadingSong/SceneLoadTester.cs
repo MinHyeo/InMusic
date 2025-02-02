507700bfd18cdb6055faaa13413c489193d7ddc5
tmddn5645 <tmddn5645@gmail.com>
2025-02-03 00:51:37 +0900

    LoadPlaySong 구현
    LoadPlaySong 구현완료
    TODO: 실제 선택한 곡을 기반으로 씬 이동하도록 Merge할 때 변경해야 함
    또한 현재 방식의 문제는 로드 관리를 위해 Resources 폴더에 Prefab을 가져오게 하거나, 미리 씬에 해당 Prefab을 호출해두고, SetActive(false)상태로 두는 방식 2개가 있음

---
+++ InMusic/Assets/Resources/SSW/Scripts/LoadingSong/SceneLoadTester.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadTester : MonoBehaviour
+{
+    [SerializeField] private GameObject loadingUIPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+            string nextScene = (currentScene == "Test1") ? "Test2" : "Test1";
+
+            GameObject loadingUI = Instantiate(loadingUIPrefab);
+            LoadingSong loadingSong = loadingUI.GetComponent<LoadingSong>();
+            loadingSong.LoadPlay(nextScene);
+            // LoadingSong.Instance.LoadPlay("Test2");
+        }
+    }
+}
