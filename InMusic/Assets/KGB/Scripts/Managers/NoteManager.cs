c634e9b1c49d8337967b5fd2f5341edda1f0094b
Tibong69 <kgb69696969@gmail.com>
2025-03-03 23:36:58 +0900

    note positon fix
    - 노트 노래에 맞게 위치 나오게 수정

--- InMusic/Assets/KGB/Scripts/Managers/NoteManager.cs
+++ InMusic/Assets/KGB/Scripts/Managers/NoteManager.cs
@@ -18,8 +18,8 @@ public class NoteManager : MonoBehaviour
     [SerializeField] GameObject songStartNote;
 
     public int lineCount = 5;          // 초기 경계선 개수
-    public float baseScrollSpeed = 5f; // 기본 스크롤 속도
-    public float bpm = 120;            // BPM (비트 당 박자 수)
+    public float baseScrollSpeed = 1f; // 기본 스크롤 속도
+    //public float bpm = 120;            // BPM (비트 당 박자 수)
     public float beatsPerMeasure = 4;  // 마디당 박자 수 (4/4 박자 기준)
     public float noteOffset = 0.15f;   // 노트 시작 오프셋
     public float secondsPerBeat;      // 1비트에 걸리는 시간(초)
@@ -71,6 +71,8 @@ public class NoteManager : MonoBehaviour
 
     public void SetNote(BMSData bms)
     {
+        baseScrollSpeed = 5f;
+
         if (bms != null)
         {
             int lastMeasure = bms.notes.Last().measure;
@@ -124,7 +126,7 @@ public class NoteManager : MonoBehaviour
                     if (noteID == "02")
                     {
                         // "02"일 때 프리팹 생성
-                        GameObject specialNote = Instantiate(songStartNote, new Vector3(xPosition, yPosition, 0), Quaternion.identity, spawnArea);
+                        GameObject specialNote = Instantiate(songStartNote, new Vector3(xPosition, yPosition-1f, 0), Quaternion.identity, spawnArea);
                         specialNote.GetComponent<ScrollDown>().SetScrollSpeed(baseScrollSpeed);
                         specialNote.SetActive(true);
                     }
