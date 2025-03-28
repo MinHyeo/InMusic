0d095b965b0573a133dca964cec558cc8000e4cd
swatper <67682393+swatper@users.noreply.github.com>
2025-03-28 22:19:46 +0900

    Squashed commit of the following:
    commit ad4ce5553c4bd9ad483366b8a62f7a3bd1cf8dfc
    Author: L-Ju123 <naver514411@gmail.com>
    Date:   Tue Mar 25 16:32:42 2025 +0900
    
        Update MainScene
    
        2025.03.25
        -수정
    
    commit 68e805d0a67f38af1dd2440cfbb511887a687e60
    Author: L-Ju123 <naver514411@gmail.com>
    Date:   Wed Mar 19 11:52:08 2025 +0900
    
        Update MainScene
    
        2025.03.18
        -기능 구현
        -코드 정리 전
    
    commit 0879904ba7b9152d847ec880f6ff43ddc7f91157
    Author: L-Ju123 <naver514411@gmail.com>
    Date:   Tue Mar 11 18:02:49 2025 +0900
    
        Update MainMenu
    
        2025.03.11
        -메인메뉴 90%완성
        -버그 픽스 필요

---
+++ InMusic/Assets/LJH/Scripts/Managers/InputManager.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager
+{
+    public Action<KeyCode> OnKeyPressed;
+
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("키 입력");
+            foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    Debug.Log("맞는 키 입력");
+                    OnKeyPressed?.Invoke(key);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ InMusic/Assets/LJH/Scripts/Utils/Define.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum DefaultKey
+    {
+        Up,
+        Left,
+        Down,
+        Right,
+        Enter,
+        F10,
+        F1,
+        Esc
+    }
+
+    public enum RhythmKey
+    {
+        Key1,
+        Key2,
+        Key3,
+        Key4
+    }
+}
