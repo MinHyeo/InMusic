a987618d83415d2dec4e7fd47436f46916a599a7
Tibong69 <kgb69696969@gmail.com>
2025-08-18 02:52:17 +0900

    테스트
    .

--- InMusic/Assets/PSH/Scripts/Player/PlayerUIController.cs
+++ InMusic/Assets/PSH/Scripts/Player/PlayerUIController.cs
@@ -1,4 +1,5 @@
 using Fusion;
+using System.Collections;
 using UnityEngine;
 using static Unity.Collections.Unicode;
 
@@ -22,9 +23,34 @@ public class PlayerUIController : NetworkBehaviour
     public void Rpc_GameStart()
     {
         GameObject waitingRoomUIManager = GameObject.Find("Waiting_Room_UI");
-        waitingRoomUIManager.GetComponent<Waiting_Room_UI>().SetBMS();
-        Runner.LoadScene(SceneRef.FromIndex(5));
+
+        //waitingRoomUIManager.GetComponent<Waiting_Room_UI>().SetBMS();
+        //Runner.LoadScene(SceneRef.FromIndex(5));
         //LoadingScreen.Instance.LoadScene("KGB_MultiPlay");
+
+        if (waitingRoomUIManager != null)
+        {
+            // SetBMS가 끝난 뒤 씬 전환
+            StartCoroutine(SetBMSAndLoadScene(waitingRoomUIManager.GetComponent<Waiting_Room_UI>()));
+        }
+    }
+
+    private IEnumerator SetBMSAndLoadScene(Waiting_Room_UI ui)
+    {
+        ui.SetBMS(); // 데이터 세팅
+        yield return null; // 혹은 SetBMS 내부 코루틴이면 끝날 때까지 yield
+
+        PlayerRef firstPlayer = default;
+        foreach (var player in Runner.ActivePlayers)
+        {
+            firstPlayer = player;
+            break; // 첫 번째만
+        }
+
+        if (Runner.LocalPlayer == firstPlayer)
+        {
+            Runner.LoadScene(SceneRef.FromIndex(5));
+        }
     }
 
     public void BroadScrollUp() {
